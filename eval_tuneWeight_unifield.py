0a3f5f641c617d6a326b3a942b599e406ba4e7b4
Liam Cripwell <ljcripwell@gmail.com>
2017-12-11 12:58:03 +1000

    made sciprt to trec eval unified fields

---
+++ eval_tuneWeight_unifield.py
@@ -0,0 +1,92 @@
+import subprocess, os
+import glob
+import multiprocessing
+import argparse
+
+parser = argparse.ArgumentParser()
+
+
+parser.add_argument("-d",
+                    "--dataset",
+                    help="CLEF2015,  CLEF2016, HARD2005, or WEB2013-2014",
+                    choices=["CLEF2015", "CLEF2016", "HARD2003", "HARD2005", "WEB2013-2014"])
+args = parser.parse_args()
+
+dataSet = args.dataset
+
+
+if dataSet == "CLEF2015":
+    qrelPath = "/volumes/ext/data/clef2015_eval/qrels.eng.clef2015.test.graded.txt"
+    topPrefix = "topTuneBoost_Clef2015_alpha"
+    resultFile = "evalTuneBoost_Clef2015.txt"
+elif dataSet == "CLEF2016":
+    qrelPath = "/volumes/ext/data/clef2016_eval/task1.qrels.30Aug"
+    topPrefix = "topTuneBoost_Clef2016_alpha"
+    resultFile = "evalTuneBoost_Clef2016.txt"
+elif dataSet == "HARD2003":
+    qrelPath = "/volumes/ext/data/hard2003_eval/qrels.actual.03.txt"
+    topPrefix = "ter_hard2003_merged_b"
+    resultFile = "ter_hard2003_tuneWeight.eval"
+elif dataSet == "HARD2005":
+    qrelPath = "/volumes/data/phd/data/aquaint_eval/TREC2005.qrels.txt"
+    topPrefix = 'ter_hard2005_a'
+    resultFile = "ter_hard2005_tuneWeight.eval"
+
+
+trecPath = "/Volumes/Data/tools/trec_eval.9.0/"
+dataPath = '/Volumes/Data/Github/ipm2017_fielded_retrieval/data/terrier/'
+
+
+def eval(fname):
+    trecResults = subprocess.getoutput(trecPath +
+                                     'trec_eval -q -m map -m P.10 -m ndcg_cut.10,1000 -m bpref -m relstring.10 '
+                                     '-m recip_rank ' + qrelPath + " " + fname)
+    #print(trecResults)
+    filename = os.path.basename(fname)
+    alpha = filename.replace(topPrefix,"").replace('.run','')
+
+    map = p10 = ndcg10 = ndcg1000 = bpref = numUnjudged = rr = "*"
+    relString = qNum = "*"
+    resultString = ""
+    for res in trecResults.splitlines():
+        measure = res.split()[0]
+        qNum = res.split()[1]
+        score = res.split()[2]
+        if qNum != "all":
+            if measure == "map":
+                map = score
+            elif measure == "bpref":
+                bpref = score
+            elif measure == "P_10":
+                p10 = score
+            elif measure == "relstring_10":
+                relString = score
+                numUnjudged = relString.count('-')
+            elif measure == "recip_rank":
+                rr = score
+            elif measure == "ndcg_cut_10":
+                ndcg10 = score
+            elif measure == "ndcg_cut_1000":
+                ndcg1000 = score
+
+                resultString += filename + " " + alpha + " " + qNum + " " + map + " " + p10 + " " + ndcg10 + " " + \
+                                ndcg1000 + " " + bpref + " " + str(numUnjudged) + " " + relString + " " + rr + "\n"
+
+    print ("File name: {0} Completed".format(filename))
+
+    return resultString
+
+fileNames = glob.glob(dataPath + topPrefix + "*.run")
+
+fw = open(dataPath + resultFile, 'w')
+fw.write("schema" + " " + "alpha" + " " + " QueryNum"+ " " + "map" + " " + "p10" + " " + "ndcg10" + " " + "ndcg1000" +
+         " " + "bpref" + " " + "unjudged" + " " + "relString" + " " + "rr""\n")
+
+p = multiprocessing.Pool(processes=5)
+resultString = p.map(eval, fileNames)
+for res in resultString:
+    fw.write(res)
+p.close()
+p.join()
+
+fw.close()
---
+++ index_clef2015.md
@@ -0,0 +1,57 @@
+# Terrier - INDEX Clueweb12B
+
+Pre-requisite:
+* Terrier 4.2 binaries (assume located in /volumes/ext/indeces/terrier-4.2)
+* Clueweb12B source files (assume located in /Volumes/ext/data/ClueWeb12/DiskB/)
+
+Working directory where we will put all config and data is:
+/Volumes/Data/Github/terrier_indexing/data/
+
+
+## 1. Generate collection.spec
+collection.spec specify list of source files to be indexed.
+```bash
+find /Volumes/ext/data/ClueWeb12/DiskB/ -type f -name '*wb-*.warc.gz'| sort |grep -v info > /volumes/ext/indeces/terrierCollectionSpecs/collection_clueweb12b_wb.spec
+```
+
+
+## 2. Create folder to hold the index
+```bash
+cd /volumes/ext/indeces/terrier-4.2/var/index
+mkdir clueweb12b
+```
+
+## 3. Index based on the generated collection.spec
+```bash
+/volumes/ext/indeces/terrier-4.2/bin/trec_terrier.sh -i -j
+-Dcollection.spec=/volumes/ext/indeces/terrierCollectionSpecs/collection_clueweb12b_wb.spec
+-Dterrier.index.path=/volumes/ext/indeces/terrier-4.2/var/index/clueweb12b/
+-DFieldTags.process=TITLE,BODY
+-Dtrec.collection.class=WARC10Collection
+-Dindexer.meta.forward.keys=docno
+-Dindexer.meta.forward.keylens=26
+-Dindexer.meta.reverse.keys=docno
+-DTrecDocTags.skip=SCRIPT,STYLE
+-Dignore.empty.documents=true
+-Dstopwords.filename=stopword-list.txt
+-Dtermpipelines=Stopwords,PorterStemmer
+-Dindexing.singlepass.max.documents.flush=10000
+-Dindexing.max.docs.per.builder=1000000
+```
+
+
+/tools/terrier-4.0/bin/trec_terrier.sh -i -j
+-Dterrier.index.path=/volumes/data/tools/terrier-4.2/var/index/clueweb12b/
+-Dcollection.spec=/Volumes/ext/indeces/terrier/collection.spec.clueweb12b13
+-DFieldTags.process=TITLE,BODY
+-Dtrec.collection.class=WARC10Collection
+-Dindexer.meta.forward.keys=docno
+-Dindexer.meta.forward.keylens=26
+-Dindexer.meta.reverse.keys=docno
+-DTrecDocTags.skip=SCRIPT,STYLE
+-Dstopwords.filename=/tools/terrier-core-4.1/share/stopword-list.txt
+-Dtermpipelines=Stopwords,TRv2PorterStemmer
+-Dindexing.singlepass.max.documents.flush=10000
+-Dindexing.max.docs.per.builder=1000000
+
+
