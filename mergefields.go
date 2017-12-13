1232257c030b055959482409dbf6403ac06978a1
Liam Cripwell <ljcripwell@gmail.com>
2017-12-13 14:00:36 +1000

    mergefields.go now reliably recreates dir structure

--- mergefields.go
+++ mergefields.go
@@ -19,14 +19,13 @@ func main() {
 	// directory of files to perform merge on
 	mergeDir := os.Args[1]
 
-	newDir := createDirectory(mergeDir)
+	newDir := mergeDir + "_MERGED" //createDirectory(mergeDir)
 
 	// get list of files to merge
 	var files []string
 	getGzs(mergeDir, &files)
 
 	// regex patterns
-	//reglimit := regexp.MustCompile("(?s)<\\/HEADLINE>.*<TEXT>")
 	regendhead := regexp.MustCompile("</HEADLINE>")
 	regstarttext := regexp.MustCompile("<TEXT>")
 	regstarthead := regexp.MustCompile("<HEADLINE>")
@@ -44,7 +43,6 @@ func main() {
 		// merge fields
 		merged := regendhead.ReplaceAllString(foo, "")
 		merged = regstarttext.ReplaceAllString(merged, "")
-		//merged := reglimit.ReplaceAllString(foo, "")
 		merged = regstarthead.ReplaceAllString(merged, "<TEXT>")
 
 		// zip and write new file
@@ -53,9 +51,15 @@ func main() {
 		w.Write([]byte(merged))
 		w.Close()
 
+		// effectively ignore parent directory
+		pieces := strings.Split(input, "/")
+		pieces = pieces[1:]
+		input = strings.Join(pieces, "/")
+		fmt.Println(input)
+
 		dirExists, _ := exists(newDir + "/" + input)
 		if !dirExists {
-			pieces := strings.Split(input, "/")
+			pieces = strings.Split(input, "/")
 			pieces = pieces[:len(pieces)-1]
 			path := strings.Join(pieces, "/")
 			createDirectory(newDir + "/" + path)
@@ -97,6 +101,15 @@ func exists(path string) (bool, error) {
 	return true, err
 }
 
+// create dir
+func createDirectory(dir string) {
+	err := os.MkdirAll(dir, 0711)
+	if err != nil {
+		log.Println("Error creating directory")
+		log.Println(err)
+	}
+}
+
 // Finds .gz files within given dir and all its children
 func getGzs(dir string, filelist *[]string) {
 	// get list of files in directory
@@ -116,93 +129,6 @@ func getGzs(dir string, filelist *[]string) {
 			}
 		}
 	}
-}
-
-func dothings() {
-	// directory of files to perform merge on
-	mergeDir := os.Args[1]
-	newDir := createDirectory(mergeDir)
-
-	// get list of files in directory
-	files, err := ioutil.ReadDir(mergeDir)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// remove files that are not of .gz format
-	for i := 0; i < len(files); i++ {
-		if files[i].Name()[len(files[i].Name())-3:] != ".gz" {
-			fmt.Println(files[i].Name()[len(files[i].Name())-3:])
-			files = append(files[:i], files[i+1:]...)
-		}
-	}
-
-	// regex patterns
-	//reglimit := regexp.MustCompile("(?s)<\\/HEADLINE>.*<TEXT>")
-	regendhead := regexp.MustCompile("</HEADLINE>")
-	regstarttext := regexp.MustCompile("<TEXT>")
-	regstarthead := regexp.MustCompile("<HEADLINE>")
-
-	// setup goroutine pool
-	numCPUs := 5                    //runtime.NumCPU()
-	runtime.GOMAXPROCS(numCPUs + 1) // numCPUs hot threads + one for async tasks.
-
-	pool, _ := tunny.CreatePool(numCPUs, func(object interface{}) interface{} {
-		input, _ := object.(string)
-
-		// read from file
-		foo := getStringFromFile(mergeDir + "/" + input)
-
-		// merge fields
-		merged := regendhead.ReplaceAllString(foo, "")
-		merged = regstarttext.ReplaceAllString(merged, "")
-		//merged := reglimit.ReplaceAllString(foo, "")
-		merged = regstarthead.ReplaceAllString(merged, "<TEXT>")
-
-		// zip and write new file
-		var b bytes.Buffer
-		w := gzip.NewWriter(&b)
-		w.Write([]byte(merged))
-		w.Close()
-		ioutil.WriteFile(newDir+"/"+input, b.Bytes(), 0666)
-
-		return input
-	}).Open()
-	defer pool.Close()
-
-	// sync group to handle all jobs
-	wg := new(sync.WaitGroup)
-	wg.Add(len(files))
-
-	// perform all merges
-	for i := 0; i < len(files); i++ {
-		go func(index int) {
-			fmt.Println("Starting " + files[index].Name())
-			value, _ := pool.SendWork(files[index].Name())
-			fmt.Println("Finished " + value.(string))
-
-			wg.Done()
-		}(i)
-	}
-
-	wg.Wait()
-}
-
-func createDirectory(dir string) string {
-	// compose name for new merged dir
-	pieces := strings.Split(dir, "/")
-	pieces[len(pieces)-1] += "_MERGED"
-	path := strings.Join(pieces, "/")
-	fmt.Println(path)
-
-	// create dir
-	err := os.MkdirAll(path, 0711)
-	if err != nil {
-		log.Println("Error creating directory")
-		log.Println(err)
-	}
-
-	return path
 }
 
 func getStringFromFile(file string) string {
