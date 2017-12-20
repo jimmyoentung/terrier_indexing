e442aee5151037b9a8ceeb71a2edb498a1568dbe
Liam Cripwell <ljcripwell@gmail.com>
2017-12-20 13:15:58 +1000

    created clueweb unifield terrier querying script

---
+++ ter_tuneBoost_Web_merged.py
@@ -0,0 +1,36 @@
+import subprocess
+import multiprocessing
+
+path_terrier = '/Volumes/ext3/indeces/terrier-4.2/bin/trec_terrier.sh'
+path_index = '/Volumes/ext3/indeces/terrier-4.2/var/index/clueweb12b_merged/'
+path_query = '/Volumes/ext/data/webTrack2013-2014_eval/web2013_2014_topics_Terrier.xml'
+prefix_run = '/Volumes/ext/liam/ter_clueweb_merged_results/ter_web_merged_b'
+
+def ter_search(query_param):
+    subprocess.call(path_terrier + query_param, shell=True)
+    print('*********** finished ***********')
+    return True
+
+# build list of query params for all alpha values
+queryParams = []
+for beta in range(0, 101, 5):
+    queryParam = ' -r ' \
+                 '-Dterrier.index.path={index} ' \
+                 '-Dtrec.topics={queryfile} ' \
+                 '-DTrecQueryTags.doctag=top ' \
+                 '-DTrecQueryTags.process=top,num,title ' \
+                 '-DTrecQueryTags.idtag=num ' \
+                 '-DTrecQueryTags.skip=desc,narr ' \
+                 '-DTrecQueryTags.casesensitive=false ' \
+                 '-Dtrec.model=BM25F ' \
+                 '-Dtrec.results.file={prefix_run}{file_id}.run ' \
+                 '-Dc.0={weight}' \
+                 '-Dw.0=1'.\
+            format(index=path_index, queryfile=path_query, prefix_run=prefix_run, file_id=beta, weight=beta/100)
+    queryParams.append(queryParam)
+
+# call terrier multi - processing
+p = multiprocessing.Pool(processes=4)
+res = p.map(ter_search, queryParams)
+
+
